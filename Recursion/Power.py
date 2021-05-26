00389ca37eb8733c74f6153dd1623792b9216b0e
NobleBlack <abhisheksharma0313@gmail.com>
2021-05-26 09:49:54 +0530

    Power Recursive Solution

--- Recursion/Power.py
+++ Recursion/Power.py
@@ -2,5 +2,16 @@ import sys
 sys.stdout = open("Recursion/../IO/output.txt", "w")
 sys.stdin = open("Recursion/../IO/input.txt", "r")
 
+n = int(input())
+# Program to print power of 2 
+def Power(n):
+  # Unintentional Case 
+  assert int(n) == n and n >= 0, "Number should be Positive Integer only"
+  
+  # Base Case
+  if (n < 1):
+    return 1  
+  else: 
+    return 2 * Power(n-1)
 
-
+print(Power(n))
